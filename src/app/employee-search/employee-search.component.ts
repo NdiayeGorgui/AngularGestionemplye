ab0f8dedfabc5036898d43c394c3c534f0a4e467
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-03-19 00:45:35 -0400

    ajout de nouvelles fonctionalités

---
+++ src/app/employee-search/employee-search.component.ts
@@ -0,0 +1,23 @@
+
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-employee-search',
+  templateUrl: './employee-search.component.html',
+  styleUrls: ['./employee-search.component.css']
+})
+export class EmployeeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSearch(dataForm: any){
+    console.log(dataForm);
+    
+    
+  }
+
+}
